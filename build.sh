452ce541a317f5265a8962169cd3137b2ef7da4a
Jamal Kheir Beik <jamalkheirbeik@gmail.com>
2026-02-17 19:05:18 +0100

    added building for freeBSD

--- build.sh
+++ build.sh
@@ -15,3 +15,9 @@ go build -o ./build/windows/certinfo_x64.exe .
 # build for windows 32-bit
 set GOARCH=386
 go build -o ./build/windows/certinfo_x32.exe .
+
+#build for freeBSD
+set GOARCH=amd64
+set GOOS=freebsd
+
+go build -o ./build/freeBSD/certinfo .
